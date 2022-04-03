149403b945fff5c7e751a87243c584ae570a684e
Lucas Cantor (he/him) <lucas@cantor.cloud>
2022-04-03 16:14:16 -0700

    Adopt imported hosted zones (#9)

--- route_53.tf
+++ route_53.tf
@@ -1,10 +1,10 @@
+# ------------------------------------------------------------------------------------------
+# Route 53 registered domains
+
 locals {
   domains_csv = file("domains.csv")
   domains     = csvdecode(local.domains_csv)
 }
-
-# ------------------------------------------------------------------------------------------
-# Route 53 registered domains
 
 resource "aws_route53domains_registered_domain" "domains" {
   for_each = { for domain in local.domains : domain.immutable_id => domain }
@@ -15,6 +15,25 @@ resource "aws_route53domains_registered_domain" "domains" {
   registrant_privacy = true
   tech_privacy       = true
   transfer_lock      = true
+
+  tags = {
+    Terraform = true
+  }
+}
+
+# ------------------------------------------------------------------------------------------
+# Route 53 hosted zones
+
+locals {
+  hosted_zones_csv = file("hosted_zones.csv")
+  hosted_zones     = csvdecode(local.hosted_zones_csv)
+}
+
+resource "aws_route53_zone" "hosted_zones" {
+  for_each = { for hosted_zone in local.hosted_zones : hosted_zone.immutable_id => hosted_zone }
+
+  name    = each.key
+  comment = var.terraform_warning
 
   tags = {
     Terraform = true
