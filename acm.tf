30f9496f619708a7da615c15836aef811a8e9dda
Lucas Cantor (he/him) <lucas@cantor.cloud>
2022-04-09 14:39:36 -0700

    Adopt imported francesca-and-lucas.com route_53 records and cert (#13)

--- acm.tf
+++ acm.tf
@@ -13,3 +13,19 @@ resource "aws_acm_certificate_validation" "lizzythepooch_com" {
   certificate_arn         = aws_acm_certificate.lizzythepooch_com.arn
   validation_record_fqdns = [for record in aws_route53_record.lizzythepooch_com__cert_validation : record.fqdn]
 }
+
+# ------------------------------------------------------------------------------------------
+# francesca-and-lucas.com
+
+resource "aws_acm_certificate" "francesca_and_lucas_com" {
+  domain_name = "francesca-and-lucas.com"
+  subject_alternative_names = [
+    "*.francesca-and-lucas.com",
+  ]
+  validation_method = "DNS"
+}
+
+resource "aws_acm_certificate_validation" "francesca_and_lucas_com" {
+  certificate_arn         = aws_acm_certificate.francesca_and_lucas_com.arn
+  validation_record_fqdns = [for record in aws_route53_record.francesca_and_lucas_com__cert_validation : record.fqdn]
+}
