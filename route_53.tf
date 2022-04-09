c2a88062d8427788bdd490cba89c10545775f20e
Lucas Cantor (he/him) <lucas@cantor.cloud>
2022-04-09 09:55:47 -0700

    Add imported lizzythepooch.com aws_route53_record resources (#11)

--- route_53.tf
+++ route_53.tf
@@ -31,3 +31,104 @@ resource "aws_route53_zone" "hosted_zones" {
   name    = each.key
   comment = var.terraform_warning
 }
+
+# ------------------------------------------------------------------------------------------
+# lizzythepooch.com
+
+resource "aws_route53_record" "lizzythepooch_com__SOA" {
+  zone_id = aws_route53_zone.hosted_zones["lizzythepooch.com"].zone_id
+  name    = "lizzythepooch.com"
+  type    = "SOA"
+  ttl     = "900"
+  records = [
+    "${aws_route53_zone.hosted_zones["lizzythepooch.com"].name_servers[1]}. awsdns-hostmaster.amazon.com. 1 7200 900 1209600 86400",
+  ]
+}
+
+resource "aws_route53_record" "lizzythepooch_com__NS" {
+  zone_id = aws_route53_zone.hosted_zones["lizzythepooch.com"].zone_id
+  name    = "lizzythepooch.com"
+  type    = "NS"
+  ttl     = "172800"
+  records = [
+    aws_route53_zone.hosted_zones["lizzythepooch.com"].name_servers[0],
+    aws_route53_zone.hosted_zones["lizzythepooch.com"].name_servers[1],
+    aws_route53_zone.hosted_zones["lizzythepooch.com"].name_servers[2],
+    aws_route53_zone.hosted_zones["lizzythepooch.com"].name_servers[3],
+  ]
+}
+
+resource "aws_route53_record" "lizzythepooch_com__A" {
+  zone_id = aws_route53_zone.hosted_zones["lizzythepooch.com"].zone_id
+  name    = "lizzythepooch.com"
+  type    = "A"
+
+  alias {
+    name                   = var.cloudfront_distribution_domain_names["lizzythepooch.com"]
+    zone_id                = var.cloudfront_distribution_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "www_lizzythepooch_com__A" {
+  zone_id = aws_route53_zone.hosted_zones["lizzythepooch.com"].zone_id
+  name    = "www.lizzythepooch.com"
+  type    = "A"
+
+  alias {
+    name                   = var.cloudfront_distribution_domain_names["lizzythepooch.com"]
+    zone_id                = var.cloudfront_distribution_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "lizzythepooch_com__MX" {
+  zone_id = aws_route53_zone.hosted_zones["lizzythepooch.com"].zone_id
+  name    = "lizzythepooch.com"
+  type    = "MX"
+  ttl     = "3600"
+  records = [
+    "0 .",
+  ]
+}
+
+resource "aws_route53_record" "lizzythepooch_com__TXT" {
+  zone_id = aws_route53_zone.hosted_zones["lizzythepooch.com"].zone_id
+  name    = "lizzythepooch.com"
+  type    = "TXT"
+  ttl     = "3600"
+  records = [
+    "v=spf1 -all",
+  ]
+}
+
+resource "aws_route53_record" "dmarc_lizzythepooch_com__TXT" {
+  zone_id = aws_route53_zone.hosted_zones["lizzythepooch.com"].zone_id
+  name    = "_dmarc.lizzythepooch.com"
+  type    = "TXT"
+  ttl     = "3600"
+  records = [
+    "v=DMARC1;p=reject;sp=reject;adkim=s;aspf=s;fo=1;rua=mailto:dmarc@lizzythepooch.com,mailto:dmarc@lizzythepooch.com",
+  ]
+}
+
+resource "aws_route53_record" "domainkey_lizzythepooch_com__TXT" {
+  zone_id = aws_route53_zone.hosted_zones["lizzythepooch.com"].zone_id
+  name    = "*._domainkey.lizzythepooch.com"
+  type    = "TXT"
+  ttl     = "3600"
+  records = [
+    "v=DKIM1; p=",
+  ]
+}
+
+resource "aws_route53_record" "ee1c094eab652e5e9dc62d5fc7e0f75b_lizzythepooch_com__CNAME" {
+  zone_id = aws_route53_zone.hosted_zones["lizzythepooch.com"].zone_id
+  name    = "_ee1c094eab652e5e9dc62d5fc7e0f75b.lizzythepooch.com"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [
+    "_00adc4c4cbe430ccd571fb87dcaf0db9.acm-validations.aws",
+  ]
+}
+
--- variables.tf
+++ variables.tf
@@ -26,3 +26,29 @@ variable "terraform_warning" {
   type        = string
   default     = "DO NOT EDIT - Managed by Terraform"
 }
+
+variable "cloudfront_distribution_zone_id" {
+  description = "Static zone ID used for all AWS CloudFront distributions"
+  type        = string
+  default     = "Z2FDTNDATAQYW2"
+}
+
+variable "cloudfront_distribution_domain_names" {
+  description = "map of existing CloudFront distribution domain names, to use temporarily during Terraform import"
+  type        = map(string)
+  default = {
+    "cantor.cloud"               = "d1zk2ijj4szow2.cloudfront.net"
+    "cdn.lucascantor.com"        = "d1tj1q9mar2m0x.cloudfront.net"
+    "centerpointwest.com"        = "d1no6tsu3mphe9.cloudfront.net"
+    "francesca-and-lucas.com"    = "d3k1gnsujs6upe.cloudfront.net"
+    "hunterscreekapartments.net" = "d3tvscov8gyjjc.cloudfront.net"
+    "kindredcode.com"            = "d2sepyl7m5lz49.cloudfront.net"
+    "lizzythepooch.com"          = "d13xp8qn1l724e.cloudfront.net"
+    "lucascantor.com"            = "d33m8g90iqvky8.cloudfront.net"
+    "blog.lucascantor.com"       = "d2kwu0a222e6pc.cloudfront.net"
+    "nathanielsigal.com"         = "d1x3378f9frm8g.cloudfront.net"
+    "ronaldcantor.com"           = "dhncpcpbkm1q0.cloudfront.net"
+    "unitizer.com"               = "d2g072sourj6ej.cloudfront.net"
+    "williamsigal.com"           = "d1qzb2dpyrtdg0.cloudfront.net"
+  }
+}
