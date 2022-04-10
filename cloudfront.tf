a3cae907f0f09a5db9452dd4d3f029a48c864334
Lucas Cantor (he/him) <lucas@cantor.cloud>
2022-04-10 15:51:09 -0700

    Adopt imported custom security headers CloudFront policy (#27)

--- cloudfront.tf
+++ cloudfront.tf
@@ -22,3 +22,40 @@ resource "aws_cloudfront_function" "url_rewrite" {
   publish = true
   code    = file("cloudfront_functions/url-rewrite.js")
 }
+
+# ------------------------------------------------------------------------------------------
+# CloudFront response headers policies
+
+resource "aws_cloudfront_response_headers_policy" "custom_security_headers_policy" {
+  name    = "CustomSecurityHeadersPolicy"
+  comment = "Adds a set of security headers to every response"
+
+  security_headers_config {
+    content_security_policy {
+      content_security_policy = "default-src 'none'; object-src 'none'; base-uri 'none'; frame-ancestors 'none'; form-action 'none'; media-src 'self'; script-src 'self'; frame-src 'self'; img-src 'self' https://cdn.lucascantor.com; style-src 'self' https://cdn.lucascantor.com https://fonts.googleapis.com https://fonts.gstatic.com; font-src 'self' https://cdn.lucascantor.com https://fonts.googleapis.com https://fonts.gstatic.com"
+      override                = true
+    }
+    content_type_options {
+      override = true
+    }
+    frame_options {
+      frame_option = "DENY"
+      override     = true
+    }
+    referrer_policy {
+      referrer_policy = "same-origin"
+      override        = true
+    }
+    strict_transport_security {
+      access_control_max_age_sec = 63072000
+      include_subdomains         = true
+      override                   = true
+      preload                    = true
+    }
+    xss_protection {
+      mode_block = true
+      override   = true
+      protection = true
+    }
+  }
+}
