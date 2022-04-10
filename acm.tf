723d5ba48f387aa6a1b7adb4ca0e593cb364cada
Lucas Cantor (he/him) <lucas@cantor.cloud>
2022-04-10 01:06:36 +0000

    Adopt imported ronaldcantor.com route_53 records and cert

--- acm.tf
+++ acm.tf
@@ -129,3 +129,35 @@ resource "aws_route53_record" "williamsigal_com__cert_validation" {
   type    = each.value.type
   zone_id = aws_route53_zone.hosted_zones[each.key].zone_id
 }
+
+# ------------------------------------------------------------------------------------------
+# ronaldcantor.com
+
+resource "aws_acm_certificate" "ronaldcantor_com" {
+  domain_name = "ronaldcantor.com"
+  subject_alternative_names = [
+    "*.ronaldcantor.com",
+  ]
+  validation_method = "DNS"
+}
+
+resource "aws_acm_certificate_validation" "ronaldcantor_com" {
+  certificate_arn         = aws_acm_certificate.ronaldcantor_com.arn
+  validation_record_fqdns = [for record in aws_route53_record.ronaldcantor_com__cert_validation : record.fqdn]
+}
+
+resource "aws_route53_record" "ronaldcantor_com__cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.ronaldcantor_com.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    } if substr(dvo.domain_name, 0, 2) != "*."
+  }
+
+  name    = each.value.name
+  records = [each.value.record]
+  ttl     = 300
+  type    = each.value.type
+  zone_id = aws_route53_zone.hosted_zones[each.key].zone_id
+}
