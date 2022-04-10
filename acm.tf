0a3a1a125793c31f61546b26344c46d9f04f6fcf
Lucas Cantor (he/him) <lucas@cantor.cloud>
2022-04-10 19:26:30 +0000

    Adopt imported unitizer.com route_53 records and cert

--- acm.tf
+++ acm.tf
@@ -295,3 +295,35 @@ resource "aws_route53_record" "lucascantor_com__cert_validation" {
   type    = each.value.type
   zone_id = aws_route53_zone.hosted_zones[each.key].zone_id
 }
+
+# ------------------------------------------------------------------------------------------
+# unitizer.com
+
+resource "aws_acm_certificate" "unitizer_com" {
+  domain_name = "unitizer.com"
+  subject_alternative_names = [
+    "*.unitizer.com",
+  ]
+  validation_method = "DNS"
+}
+
+resource "aws_acm_certificate_validation" "unitizer_com" {
+  certificate_arn         = aws_acm_certificate.unitizer_com.arn
+  validation_record_fqdns = [for record in aws_route53_record.unitizer_com__cert_validation : record.fqdn]
+}
+
+resource "aws_route53_record" "unitizer_com__cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.unitizer_com.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    } if substr(dvo.domain_name, 0, 2) != "*."
+  }
+
+  name    = each.value.name
+  records = [each.value.record]
+  ttl     = 300
+  type    = each.value.type
+  zone_id = aws_route53_zone.hosted_zones[each.key].zone_id
+}
