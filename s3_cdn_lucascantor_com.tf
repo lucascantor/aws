4798c4a56449c0a8d185cb2df0a7d4e979c3491b
Lucas Cantor (he/him) <lucas@cantor.cloud>
2022-04-16 22:59:23 +0000

    add file

---
+++ s3_cdn_lucascantor_com.tf
@@ -0,0 +1,20 @@
+# ------------------------------------------------------------------------------------------
+# S3 Bucket CORS configuration
+
+resource "aws_s3_bucket_cors_configuration" "cdn_lucascantor_com_cors_configuration" {
+  bucket = aws_s3_bucket.s3_buckets["cdn.lucascantor.com"].id
+
+  cors_rule {
+    allowed_headers = [
+      "Authorization",
+    ]
+    allowed_methods = [
+      "GET",
+    ]
+    allowed_origins = [
+      "*",
+    ]
+    expose_headers  = []
+    max_age_seconds = 3000
+  }
+}
