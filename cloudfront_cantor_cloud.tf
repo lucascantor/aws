4a0dc783c361201b976395b51e8b11bd70f3349c
Lucas Cantor (he/him) <lucas@cantor.cloud>
2022-04-16 15:26:56 +0000

    Adopt imported cantor.cloud CloudFront distribution

---
+++ cloudfront_cantor_cloud.tf
@@ -0,0 +1,57 @@
+# ------------------------------------------------------------------------------------------
+# cantor.cloud
+
+resource "aws_cloudfront_distribution" "cantor_cloud" {
+  aliases = [
+    "cantor.cloud",
+    "www.cantor.cloud",
+  ]
+  custom_error_response {
+    error_caching_min_ttl = 300
+    error_code            = 403
+    response_code         = 403
+    response_page_path    = "/403.html"
+  }
+  custom_error_response {
+    error_caching_min_ttl = 300
+    error_code            = 404
+    response_code         = 404
+    response_page_path    = "/404.html"
+  }
+  default_root_object = "index.html"
+  default_cache_behavior {
+    allowed_methods = [
+      "GET",
+      "HEAD",
+    ]
+    cached_methods = [
+      "GET",
+      "HEAD",
+    ]
+    cache_policy_id            = local.managed_cloudfront_caching_optimized_policy_id
+    compress                   = true
+    response_headers_policy_id = aws_cloudfront_response_headers_policy.custom_security_headers_policy.id
+    target_origin_id           = "S3-cantor.cloud"
+    viewer_protocol_policy     = "redirect-to-https"
+  }
+  enabled         = true
+  is_ipv6_enabled = true
+  origin {
+    domain_name = "cantor.cloud.s3.amazonaws.com"
+    origin_id   = "S3-cantor.cloud"
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.identities["cantor.cloud"].cloudfront_access_identity_path
+    }
+  }
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate.cantor_cloud.arn
+    minimum_protocol_version = "TLSv1.2_2021"
+    ssl_support_method       = "sni-only"
+  }
+}
