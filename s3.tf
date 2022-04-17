201766709db7a904f7c713807fff24a2e7f037ce
Lucas Cantor (he/him) <lucas@cantor.cloud>
2022-04-17 00:36:30 -0700

    Set MIME types for aws_s3_object resources (#56)

--- s3.tf
+++ s3.tf
@@ -1,6 +1,8 @@
 locals {
-  s3_buckets_csv = file("s3_buckets.csv")
-  s3_buckets     = csvdecode(local.s3_buckets_csv)
+  s3_buckets_csv  = file("s3_buckets.csv")
+  s3_buckets      = csvdecode(local.s3_buckets_csv)
+  mime_types_json = file("mime_types.json")
+  mime_types      = jsondecode(local.mime_types_json)
 }
 
 # ------------------------------------------------------------------------------------------
@@ -71,9 +73,10 @@ data "aws_iam_policy_document" "policy_for_cloudfront_private_content" {
 # S3 objects for all websites
 
 resource "aws_s3_object" "websites" {
-  for_each = fileset("websites/", "**")
-  bucket   = regex("^[^/]*", each.value)
-  key      = regex("/.*$", each.value)
-  source   = "websites/${each.value}"
-  etag     = filemd5("websites/${each.value}")
+  for_each     = fileset("websites/", "**")
+  bucket       = regex("^[^/]*", each.value)
+  content_type = lookup(local.mime_types, regex("\\.[^.]+$", each.value), null)
+  etag         = filemd5("websites/${each.value}")
+  key          = regex("/.*$", each.value)
+  source       = "websites/${each.value}"
 }
