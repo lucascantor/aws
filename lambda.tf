6987bca7e350d7e62955001f14da2873eab9ac03
Lucas Cantor <lucas@cantor.cloud>
2022-04-17 10:06:05 -0700

    Set handler

--- lambda.tf
+++ lambda.tf
@@ -23,6 +23,7 @@ resource "aws_iam_role" "policy_for_cloudfront_invalidation_lambda" {
 resource "aws_lambda_function" "cloudfront_invalidation_lambda" {
   filename         = "lambda_functions/lambda_invalidate_cloudfront.zip"
   function_name    = "lambda_function_name"
+  handler          = "lambda_invalidate_cloudfront.lambda_handler"
   runtime          = "python3.9"
   role             = aws_iam_role.policy_for_cloudfront_invalidation_lambda.arn
   source_code_hash = filebase64sha256("lambda_functions/lambda_invalidate_cloudfront.zip")
