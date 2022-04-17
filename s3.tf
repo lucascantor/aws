4aa9f3cd06323c8670dc829a45aad7d8a4a0ba9b
Lucas Cantor <lucas@cantor.cloud>
2022-04-17 11:19:46 -0700

    Use templatefile

--- s3.tf
+++ s3.tf
@@ -1,5 +1,19 @@
 locals {
-  s3_buckets_csv  = file("s3_buckets.csv")
+  s3_buckets_csv = templatefile("s3_buckets.csv", {
+    backup_cantor_cloud_cloudfront_distribution_id        = aws_cloudfront_distribution.backup_cantor_cloud.id,
+    cantor_cloud_cloudfront_distribution_id               = aws_cloudfront_distribution.cantor_cloud.id,
+    cdn_lucascantor_com_cloudfront_distribution_id        = aws_cloudfront_distribution.cdn_lucascantor_com.id,
+    centerpointwest_com_cloudfront_distribution_id        = aws_cloudfront_distribution.centerpointwest_com.id,
+    francesca_and_lucas_com_cloudfront_distribution_id    = aws_cloudfront_distribution.francesca_and_lucas_com.id,
+    hunterscreekapartments_net_cloudfront_distribution_id = aws_cloudfront_distribution.hunterscreekapartments_net.id,
+    kindredcode_com_cloudfront_distribution_id            = aws_cloudfront_distribution.kindredcode_com.id,
+    lizzythepooch_com_cloudfront_distribution_id          = aws_cloudfront_distribution.lizzythepooch_com.id,
+    lucascantor_com_cloudfront_distribution_id            = aws_cloudfront_distribution.lucascantor_com.id,
+    nathanielsigal_com_cloudfront_distribution_id         = aws_cloudfront_distribution.nathanielsigal_com.id,
+    ronaldcantor_com_cloudfront_distribution_id           = aws_cloudfront_distribution.ronaldcantor_com.id,
+    unitizer_com_cloudfront_distribution_id               = aws_cloudfront_distribution.unitizer_com.id,
+    williamsigal_com_cloudfront_distribution_id           = aws_cloudfront_distribution.williamsigal_com.id,
+  })
   s3_buckets      = csvdecode(local.s3_buckets_csv)
   mime_types_json = file("mime_types.json")
   mime_types      = jsondecode(local.mime_types_json)
@@ -23,7 +37,7 @@ resource "aws_s3_bucket" "s3_buckets" {
 
   bucket = each.key
   tags = {
-    distribution_id = try("aws_cloudfront_distribution.${each.value.cloudfront_distribution}.id", null)
+    distribution_id = try(each.value.cloudfront_distribution_id, null)
   }
 }
 
