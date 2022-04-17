26e7c4b86f10b1f38574906c40c2965ac7c307d9
Lucas Cantor (he/him) <lucas@cantor.cloud>
2022-04-17 11:29:58 -0700

    Invalidate CloudFront when site content changes (#58)
    Use aws_s3_bucket_notification resources to trigger aws_lambda_function.cloudfront_invalidation_lambda

--- s3.tf
+++ s3.tf
@@ -1,5 +1,18 @@
 locals {
-  s3_buckets_csv  = file("s3_buckets.csv")
+  s3_buckets_csv = templatefile("s3_buckets.csv", {
+    cantor_cloud_cloudfront_distribution_id               = aws_cloudfront_distribution.cantor_cloud.id,
+    cdn_lucascantor_com_cloudfront_distribution_id        = aws_cloudfront_distribution.cdn_lucascantor_com.id,
+    centerpointwest_com_cloudfront_distribution_id        = aws_cloudfront_distribution.centerpointwest_com.id,
+    francesca_and_lucas_com_cloudfront_distribution_id    = aws_cloudfront_distribution.francesca_and_lucas_com.id,
+    hunterscreekapartments_net_cloudfront_distribution_id = aws_cloudfront_distribution.hunterscreekapartments_net.id,
+    kindredcode_com_cloudfront_distribution_id            = aws_cloudfront_distribution.kindredcode_com.id,
+    lizzythepooch_com_cloudfront_distribution_id          = aws_cloudfront_distribution.lizzythepooch_com.id,
+    lucascantor_com_cloudfront_distribution_id            = aws_cloudfront_distribution.lucascantor_com.id,
+    nathanielsigal_com_cloudfront_distribution_id         = aws_cloudfront_distribution.nathanielsigal_com.id,
+    ronaldcantor_com_cloudfront_distribution_id           = aws_cloudfront_distribution.ronaldcantor_com.id,
+    unitizer_com_cloudfront_distribution_id               = aws_cloudfront_distribution.unitizer_com.id,
+    williamsigal_com_cloudfront_distribution_id           = aws_cloudfront_distribution.williamsigal_com.id,
+  })
   s3_buckets      = csvdecode(local.s3_buckets_csv)
   mime_types_json = file("mime_types.json")
   mime_types      = jsondecode(local.mime_types_json)
@@ -22,6 +35,26 @@ resource "aws_s3_bucket" "s3_buckets" {
   for_each = { for bucket in local.s3_buckets : bucket.immutable_id => bucket }
 
   bucket = each.key
+  tags = {
+    distribution_id = try(each.value.cloudfront_distribution_id, null)
+  }
+}
+
+# ------------------------------------------------------------------------------------------
+# S3 Bucket notifications to invalidate associated CloudFront distributions
+resource "aws_s3_bucket_notification" "cloudfront_invalidation_lambda" {
+  for_each = { for bucket in local.s3_buckets : bucket.immutable_id => bucket
+    if contains(local.websites[*].immutable_id, bucket.immutable_id)
+  }
+
+  bucket = each.key
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.cloudfront_invalidation_lambda.arn
+    events = [
+      "s3:ObjectCreated:*",
+      "s3:ObjectRemoved:*"
+    ]
+  }
 }
 
 # ------------------------------------------------------------------------------------------
@@ -73,7 +106,8 @@ data "aws_iam_policy_document" "policy_for_cloudfront_private_content" {
 # S3 objects for all websites
 
 resource "aws_s3_object" "websites" {
-  for_each     = fileset("websites/", "**")
+  for_each = fileset("websites/", "**")
+
   bucket       = regex("^[^/]*", each.value)
   content_type = lookup(local.mime_types, regex("\\.[^.]+$", each.value), null)
   etag         = filemd5("websites/${each.value}")
