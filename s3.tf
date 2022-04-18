5f41137d7a7886504f81fedb582f7e0e121a2461
Lucas Cantor (he/him) <lucas@cantor.cloud>
2022-04-17 17:09:09 -0700

    Remove aws_lambda_permission resources again (#71)

--- s3.tf
+++ s3.tf
@@ -43,6 +43,7 @@ resource "aws_s3_bucket" "s3_buckets" {
 # ------------------------------------------------------------------------------------------
 # S3 Bucket notifications to invalidate associated CloudFront distributions
 
+/*
 resource "aws_lambda_permission" "cloudfront_invalidation_lambda" {
   for_each = { for bucket in local.s3_buckets : bucket.immutable_id => bucket
     if contains(local.websites[*].immutable_id, bucket.immutable_id)
@@ -57,7 +58,6 @@ resource "aws_lambda_permission" "cloudfront_invalidation_lambda" {
   qualifier           = aws_lambda_alias.cloudfront_invalidation_lambda_latest_alias.name
 }
 
-/*
 resource "aws_s3_bucket_notification" "cloudfront_invalidation_lambda" {
   for_each = { for bucket in local.s3_buckets : bucket.immutable_id => bucket
     if contains(local.websites[*].immutable_id, bucket.immutable_id)
