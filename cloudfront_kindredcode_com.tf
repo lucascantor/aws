831477fe5f76a3bad33261c8f4239dd940742b8e
Lucas Cantor <lucas@cantor.cloud>
2023-01-09 16:14:20 -0800

    associate url-rewrite cloudfront function with kindredcode.com
    also fix typo

--- cloudfront_kindredcode_com.tf
+++ cloudfront_kindredcode_com.tf
@@ -32,8 +32,12 @@ resource "aws_cloudfront_distribution" "kindredcode_com" {
       "GET",
       "HEAD",
     ]
-    cache_policy_id            = local.managed_cloudfront_caching_optimized_policy_id
-    compress                   = true
+    cache_policy_id = local.managed_cloudfront_caching_optimized_policy_id
+    compress        = true
+    function_association {
+      event_type   = "viewer-request"
+      function_arn = aws_cloudfront_function.url_rewrite.arn
+    }
     response_headers_policy_id = aws_cloudfront_response_headers_policy.custom_security_headers_policy.id
     target_origin_id           = "S3-kindredcode.com"
     viewer_protocol_policy     = "redirect-to-https"
