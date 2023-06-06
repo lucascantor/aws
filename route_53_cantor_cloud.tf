a3d46b67470511a62034c34e63a3fd2e082ec9b4
Lucas Cantor <lucas@cantor.cloud>
2023-06-06 10:46:32 -0700

    remove microsoft dns records (#125)
    migration to google proceeding

--- route_53_cantor_cloud.tf
+++ route_53_cantor_cloud.tf
@@ -64,7 +64,7 @@ resource "aws_route53_record" "cantor_cloud__TXT" {
   type    = "TXT"
   ttl     = "3600"
   records = [
-    "v=spf1 include:_spf.google.com include:spf.protection.outlook.com ~all",
+    "v=spf1 include:_spf.google.com ~all",
     "google-site-verification=uuX66DHR88MjdyQ4UxPoNQyYSazX68cR166T8-vOuX8",
   ]
 }
@@ -86,95 +86,5 @@ resource "aws_route53_record" "google_domainkey_cantor_cloud__TXT" {
   ttl     = "3600"
   records = [
     "v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCaX6HMcezMI6MnOpByy3PouRWDvwq86Sm+ZxDlZSXewyEoYR/dpEmHsW7vNq3n5kvFWx0I1ZhLInl+pNietrHHb0KE+Q9AXFJfNeV+f+tztS4ZjbpTvsg75mXmbvsuXSL9uTPpg+YoR3+qirQQ7Yhkq7QGDiHOevh84MljuO25MwIDAQAB",
-  ]
-}
-
-resource "aws_route53_record" "selector1_domainkey_cantor_cloud__CNAME" {
-  zone_id = aws_route53_zone.hosted_zones["cantor.cloud"].zone_id
-  name    = "selector1._domainkey.cantor.cloud"
-  type    = "CNAME"
-  ttl     = "3600"
-  records = [
-    "selector1-cantor-cloud._domainkey.cantorcloud.onmicrosoft.com",
-  ]
-}
-
-resource "aws_route53_record" "selector2_domainkey_cantor_cloud__CNAME" {
-  zone_id = aws_route53_zone.hosted_zones["cantor.cloud"].zone_id
-  name    = "selector2._domainkey.cantor.cloud"
-  type    = "CNAME"
-  ttl     = "3600"
-  records = [
-    "selector2-cantor-cloud._domainkey.cantorcloud.onmicrosoft.com",
-  ]
-}
-
-resource "aws_route53_record" "autodiscover_cantor_cloud__CNAME" {
-  zone_id = aws_route53_zone.hosted_zones["cantor.cloud"].zone_id
-  name    = "autodiscover.cantor.cloud"
-  type    = "CNAME"
-  ttl     = "3600"
-  records = [
-    "autodiscover.outlook.com.",
-  ]
-}
-
-resource "aws_route53_record" "enterpriseenrollment_cantor_cloud__CNAME" {
-  zone_id = aws_route53_zone.hosted_zones["cantor.cloud"].zone_id
-  name    = "enterpriseenrollment.cantor.cloud"
-  type    = "CNAME"
-  ttl     = "3600"
-  records = [
-    "enterpriseenrollment.manage.microsoft.com.",
-  ]
-}
-
-resource "aws_route53_record" "enterpriseregistration_cantor_cloud__CNAME" {
-  zone_id = aws_route53_zone.hosted_zones["cantor.cloud"].zone_id
-  name    = "enterpriseregistration.cantor.cloud"
-  type    = "CNAME"
-  ttl     = "3600"
-  records = [
-    "enterpriseregistration.windows.net.",
-  ]
-}
-
-resource "aws_route53_record" "lyncdiscover_cantor_cloud__CNAME" {
-  zone_id = aws_route53_zone.hosted_zones["cantor.cloud"].zone_id
-  name    = "lyncdiscover.cantor.cloud"
-  type    = "CNAME"
-  ttl     = "3600"
-  records = [
-    "webdir.online.lync.com.",
-  ]
-}
-
-resource "aws_route53_record" "sip_cantor_cloud__CNAME" {
-  zone_id = aws_route53_zone.hosted_zones["cantor.cloud"].zone_id
-  name    = "sip.cantor.cloud"
-  type    = "CNAME"
-  ttl     = "3600"
-  records = [
-    "sipdir.online.lync.com.",
-  ]
-}
-
-resource "aws_route53_record" "sipfederationtls_tcp_cantor_cloud__SRV" {
-  zone_id = aws_route53_zone.hosted_zones["cantor.cloud"].zone_id
-  name    = "_sipfederationtls._tcp.cantor.cloud"
-  type    = "SRV"
-  ttl     = "3600"
-  records = [
-    "100 1 5061 sipfed.online.lync.com.",
-  ]
-}
-
-resource "aws_route53_record" "sip_tls_cantor_cloud__SRV" {
-  zone_id = aws_route53_zone.hosted_zones["cantor.cloud"].zone_id
-  name    = "_sip._tls.cantor.cloud"
-  type    = "SRV"
-  ttl     = "3600"
-  records = [
-    "100 1 443 sipdir.online.lync.com.",
   ]
 }
--- route_53_kindredcode_com.tf
+++ route_53_kindredcode_com.tf
@@ -64,7 +64,7 @@ resource "aws_route53_record" "kindredcode_com__TXT" {
   type    = "TXT"
   ttl     = "3600"
   records = [
-    "v=spf1 include:_spf.google.com include:spf.protection.outlook.com ~all",
+    "v=spf1 include:_spf.google.com ~all",
     "google-site-verification=iCA61zpmcK-WMakf4kxbB4SCE2r0P1knduvt3skSHwY",
   ]
 }
@@ -96,95 +96,5 @@ resource "aws_route53_record" "github_challenge_kindredcode_kindredcode_com__TXT
   ttl     = "3600"
   records = [
     "6bc9fd3749",
-  ]
-}
-
-resource "aws_route53_record" "selector1_domainkey_kindredcode_com__CNAME" {
-  zone_id = aws_route53_zone.hosted_zones["kindredcode.com"].zone_id
-  name    = "selector1._domainkey.kindredcode.com"
-  type    = "CNAME"
-  ttl     = "3600"
-  records = [
-    "selector1-kindredcode-com._domainkey.cantorcloud.onmicrosoft.com",
-  ]
-}
-
-resource "aws_route53_record" "selector2_domainkey_kindredcode_com__CNAME" {
-  zone_id = aws_route53_zone.hosted_zones["kindredcode.com"].zone_id
-  name    = "selector2._domainkey.kindredcode.com"
-  type    = "CNAME"
-  ttl     = "3600"
-  records = [
-    "selector2-kindredcode-com._domainkey.cantorcloud.onmicrosoft.com",
-  ]
-}
-
-resource "aws_route53_record" "autodiscover_kindredcode_com__CNAME" {
-  zone_id = aws_route53_zone.hosted_zones["kindredcode.com"].zone_id
-  name    = "autodiscover.kindredcode.com"
-  type    = "CNAME"
-  ttl     = "3600"
-  records = [
-    "autodiscover.outlook.com.",
-  ]
-}
-
-resource "aws_route53_record" "enterpriseenrollment_kindredcode_com__CNAME" {
-  zone_id = aws_route53_zone.hosted_zones["kindredcode.com"].zone_id
-  name    = "enterpriseenrollment.kindredcode.com"
-  type    = "CNAME"
-  ttl     = "3600"
-  records = [
-    "enterpriseenrollment.manage.microsoft.com.",
-  ]
-}
-
-resource "aws_route53_record" "enterpriseregistration_kindredcode_com__CNAME" {
-  zone_id = aws_route53_zone.hosted_zones["kindredcode.com"].zone_id
-  name    = "enterpriseregistration.kindredcode.com"
-  type    = "CNAME"
-  ttl     = "3600"
-  records = [
-    "enterpriseregistration.windows.net.",
-  ]
-}
-
-resource "aws_route53_record" "lyncdiscover_kindredcode_com__CNAME" {
-  zone_id = aws_route53_zone.hosted_zones["kindredcode.com"].zone_id
-  name    = "lyncdiscover.kindredcode.com"
-  type    = "CNAME"
-  ttl     = "3600"
-  records = [
-    "webdir.online.lync.com.",
-  ]
-}
-
-resource "aws_route53_record" "sip_kindredcode_com__CNAME" {
-  zone_id = aws_route53_zone.hosted_zones["kindredcode.com"].zone_id
-  name    = "sip.kindredcode.com"
-  type    = "CNAME"
-  ttl     = "3600"
-  records = [
-    "sipdir.online.lync.com.",
-  ]
-}
-
-resource "aws_route53_record" "sipfederationtls_tcp_kindredcode_com__SRV" {
-  zone_id = aws_route53_zone.hosted_zones["kindredcode.com"].zone_id
-  name    = "_sipfederationtls._tcp.kindredcode.com"
-  type    = "SRV"
-  ttl     = "3600"
-  records = [
-    "100 1 5061 sipfed.online.lync.com.",
-  ]
-}
-
-resource "aws_route53_record" "sip_tls_kindredcode_com__SRV" {
-  zone_id = aws_route53_zone.hosted_zones["kindredcode.com"].zone_id
-  name    = "_sip._tls.kindredcode.com"
-  type    = "SRV"
-  ttl     = "3600"
-  records = [
-    "100 1 443 sipdir.online.lync.com.",
   ]
 }
