c6d5a23fb4df8a94382b2281dfe530340e223da5
Lucas Cantor <lucas@cantor.cloud>
2024-04-19 10:18:09 -0700

    continue report-to deployment

--- cloudfront_unitizer_com.tf
+++ cloudfront_unitizer_com.tf
@@ -18,7 +18,7 @@ resource "aws_cloudfront_distribution" "unitizer_com" {
     ]
     cache_policy_id            = local.managed_cloudfront_caching_optimized_policy_id
     compress                   = true
-    response_headers_policy_id = aws_cloudfront_response_headers_policy.custom_response_headers_policy_legacy.id
+    response_headers_policy_id = aws_cloudfront_response_headers_policy.custom_response_headers_policy_default.id
     target_origin_id           = "S3-unitizer.com"
     viewer_protocol_policy     = "redirect-to-https"
   }
