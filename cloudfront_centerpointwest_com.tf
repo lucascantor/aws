2c2a874a06d19006c6365bbc5878f0c47cb00079
Lucas Cantor <lucas@cantor.cloud>
2024-05-03 10:33:18 -0700

    enable mta-sts for secondary domains

--- cloudfront_centerpointwest_com.tf
+++ cloudfront_centerpointwest_com.tf
@@ -62,3 +62,93 @@ resource "aws_cloudfront_distribution" "centerpointwest_com" {
     ssl_support_method       = "sni-only"
   }
 }
+
+# ------------------------------------------------------------------------------------------
+# mta-sts.centerpointwest.com
+
+resource "aws_cloudfront_distribution" "mta_sts_centerpointwest_com" {
+  aliases = [
+    "mta-sts.centerpointwest.com",
+  ]
+  default_root_object = "index.html"
+  default_cache_behavior {
+    allowed_methods = [
+      "GET",
+      "HEAD",
+    ]
+    cached_methods = [
+      "GET",
+      "HEAD",
+    ]
+    cache_policy_id            = local.managed_cloudfront_caching_optimized_policy_id
+    compress                   = true
+    response_headers_policy_id = aws_cloudfront_response_headers_policy.custom_response_headers_policy_cdn.id
+    target_origin_id           = "S3-mta-sts.centerpointwest.com"
+    viewer_protocol_policy     = "redirect-to-https"
+  }
+  enabled         = true
+  is_ipv6_enabled = true
+  origin {
+    domain_name = "mta-sts.centerpointwest.com.s3.amazonaws.com"
+    origin_id   = "S3-mta-sts.centerpointwest.com"
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.identities["mta-sts.centerpointwest.com"].cloudfront_access_identity_path
+    }
+  }
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate.centerpointwest_com.arn
+    minimum_protocol_version = "TLSv1.2_2021"
+    ssl_support_method       = "sni-only"
+  }
+}
+
+# ------------------------------------------------------------------------------------------
+# mta-sts.centerpointwestapartments.com
+
+resource "aws_cloudfront_distribution" "mta_sts_centerpointwestapartments_com" {
+  aliases = [
+    "mta-sts.centerpointwestapartments.com",
+  ]
+  default_root_object = "index.html"
+  default_cache_behavior {
+    allowed_methods = [
+      "GET",
+      "HEAD",
+    ]
+    cached_methods = [
+      "GET",
+      "HEAD",
+    ]
+    cache_policy_id            = local.managed_cloudfront_caching_optimized_policy_id
+    compress                   = true
+    response_headers_policy_id = aws_cloudfront_response_headers_policy.custom_response_headers_policy_cdn.id
+    target_origin_id           = "S3-mta-sts.centerpointwestapartments.com"
+    viewer_protocol_policy     = "redirect-to-https"
+  }
+  enabled         = true
+  is_ipv6_enabled = true
+  origin {
+    domain_name = "mta-sts.centerpointwestapartments.com.s3.amazonaws.com"
+    origin_id   = "S3-mta-sts.centerpointwestapartments.com"
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.identities["mta-sts.centerpointwestapartments.com"].cloudfront_access_identity_path
+    }
+  }
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate.centerpointwest_com.arn
+    minimum_protocol_version = "TLSv1.2_2021"
+    ssl_support_method       = "sni-only"
+  }
+}
--- cloudfront_kindredcode_com.tf
+++ cloudfront_kindredcode_com.tf
@@ -63,3 +63,48 @@ resource "aws_cloudfront_distribution" "kindredcode_com" {
     ssl_support_method       = "sni-only"
   }
 }
+
+# ------------------------------------------------------------------------------------------
+# mta-sts.kindredcode.com
+
+resource "aws_cloudfront_distribution" "mta_sts_kindredcode_com" {
+  aliases = [
+    "mta-sts.kindredcode.com",
+  ]
+  default_root_object = "index.html"
+  default_cache_behavior {
+    allowed_methods = [
+      "GET",
+      "HEAD",
+    ]
+    cached_methods = [
+      "GET",
+      "HEAD",
+    ]
+    cache_policy_id            = local.managed_cloudfront_caching_optimized_policy_id
+    compress                   = true
+    response_headers_policy_id = aws_cloudfront_response_headers_policy.custom_response_headers_policy_cdn.id
+    target_origin_id           = "S3-mta-sts.kindredcode.com"
+    viewer_protocol_policy     = "redirect-to-https"
+  }
+  enabled         = true
+  is_ipv6_enabled = true
+  origin {
+    domain_name = "mta-sts.kindredcode.com.s3.amazonaws.com"
+    origin_id   = "S3-mta-sts.kindredcode.com"
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.identities["mta-sts.kindredcode.com"].cloudfront_access_identity_path
+    }
+  }
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate.kindredcode_com.arn
+    minimum_protocol_version = "TLSv1.2_2021"
+    ssl_support_method       = "sni-only"
+  }
+}
