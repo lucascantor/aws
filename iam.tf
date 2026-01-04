b9ee2b13564bed6c64f451e88b2d5576c638e58d
Lucas Cantor <lucas@cantor.cloud>
2026-01-03 20:53:04 -0800

    trim pr comments based on different criteria
    remove all terraform cloud resources

--- iam.tf
+++ iam.tf
@@ -20,18 +20,6 @@ resource "aws_iam_openid_connect_provider" "github_actions" {
   thumbprint_list = [data.tls_certificate.github_actions_certificate.certificates[0].sha1_fingerprint]
 }
 
-# Terraform Cloud OIDC identity provider TLS certificate
-data "tls_certificate" "tfc_certificate" {
-  url = "https://${var.tfc_hostname}"
-}
-
-# Terraform Cloud OIDC identity provider
-resource "aws_iam_openid_connect_provider" "terraform_cloud" {
-  url             = data.tls_certificate.tfc_certificate.url
-  client_id_list  = [var.tfc_aws_audience]
-  thumbprint_list = [data.tls_certificate.tfc_certificate.certificates[0].sha1_fingerprint]
-}
-
 # ------------------------------------------------------------------------------------------
 # IAM users
 
@@ -61,32 +49,6 @@ resource "aws_iam_group_membership" "admin" {
 # ------------------------------------------------------------------------------------------
 # IAM roles
 
-# Role for GitHub Actions to assume for the blog.lucascantor.com repo
-resource "aws_iam_role" "github_actions_sts_assumption_role_blog_lucascantor_com" {
-  name = "github-actions-sts-assumption-role-blog-lucascantor-com"
-
-  assume_role_policy = <<EOF
-{
- "Version": "2012-10-17",
- "Statement": [
-   {
-     "Effect": "Allow",
-     "Principal": {
-       "Federated": "${aws_iam_openid_connect_provider.github_actions.arn}"
-     },
-     "Action": "sts:AssumeRoleWithWebIdentity",
-     "Condition": {
-       "StringEquals": {
-         "token.actions.githubusercontent.com:sub": "repo:${var.github_actions_organization_name}/${var.github_actions_repo_name_blog_lucascantor_com}:ref:refs/heads/${var.github_actions_branch_name_blog_lucascantor_com}",
-         "token.actions.githubusercontent.com:aud": "${var.github_actions_aws_audience}"
-       }
-     }
-   }
- ]
-}
-EOF
-}
-
 # Role for GitHub Actions to assume for the aws repo
 resource "aws_iam_role" "github_actions_sts_assumption_role_aws" {
   name = "github-actions-sts-assumption-role-aws"
@@ -115,9 +77,9 @@ resource "aws_iam_role" "github_actions_sts_assumption_role_aws" {
 EOF
 }
 
-# Role for Terraform Cloud to assume
-resource "aws_iam_role" "terraform_cloud_sts_assumption_role" {
-  name = "terraform-cloud-sts-assumption-role"
+# Role for GitHub Actions to assume for the blog.lucascantor.com repo
+resource "aws_iam_role" "github_actions_sts_assumption_role_blog_lucascantor_com" {
+  name = "github-actions-sts-assumption-role-blog-lucascantor-com"
 
   assume_role_policy = <<EOF
 {
@@ -126,15 +88,13 @@ resource "aws_iam_role" "terraform_cloud_sts_assumption_role" {
    {
      "Effect": "Allow",
      "Principal": {
-       "Federated": "${aws_iam_openid_connect_provider.terraform_cloud.arn}"
+       "Federated": "${aws_iam_openid_connect_provider.github_actions.arn}"
      },
      "Action": "sts:AssumeRoleWithWebIdentity",
      "Condition": {
        "StringEquals": {
-         "${var.tfc_hostname}:aud": "${one(aws_iam_openid_connect_provider.terraform_cloud.client_id_list)}"
-       },
-       "StringLike": {
-         "${var.tfc_hostname}:sub": "organization:${var.tfc_organization_name}:project:${var.tfc_project_name}:workspace:${var.tfc_workspace_name}:run_phase:*"
+         "token.actions.githubusercontent.com:sub": "repo:${var.github_actions_organization_name}/${var.github_actions_repo_name_blog_lucascantor_com}:ref:refs/heads/${var.github_actions_branch_name_blog_lucascantor_com}",
+         "token.actions.githubusercontent.com:aud": "${var.github_actions_aws_audience}"
        }
      }
    }
@@ -179,12 +139,6 @@ data "aws_iam_policy_document" "blog_lucascantor_com_s3" {
 # Attachment of AWS-managed admin policy to admin group
 resource "aws_iam_group_policy_attachment" "admin_group_admin_policy" {
   group      = aws_iam_group.admin.name
-  policy_arn = data.aws_iam_policy.admin.arn
-}
-
-# Attachment of AWS-managed admin policy to Terraform Cloud STS assumption role
-resource "aws_iam_role_policy_attachment" "terraform_cloud_sts_assumption_role_admin_policy" {
-  role       = aws_iam_role.terraform_cloud_sts_assumption_role.name
   policy_arn = data.aws_iam_policy.admin.arn
 }
 
--- variables.tf
+++ variables.tf
@@ -37,36 +37,6 @@ variable "github_actions_branch_name_blog_lucascantor_com" {
   description = "The name of the GitHub repo branch to use with AWS"
 }
 
-variable "tfc_aws_audience" {
-  type        = string
-  default     = "aws.workload.identity"
-  description = "The audience value to use in run identity tokens"
-}
-
-variable "tfc_hostname" {
-  type        = string
-  default     = "app.terraform.io"
-  description = "The hostname of the TFC or TFE instance to use with AWS"
-}
-
-variable "tfc_organization_name" {
-  type        = string
-  default     = "unitizer"
-  description = "The name of the Terraform Cloud organization to use with AWS"
-}
-
-variable "tfc_project_name" {
-  type        = string
-  default     = "Default Project"
-  description = "The name of the Terraform Cloud project under which a workspace will be used"
-}
-
-variable "tfc_workspace_name" {
-  type        = string
-  default     = "aws"
-  description = "The name of the Terraform Cloud workspace to connect to AWS"
-}
-
 variable "region" {
   description = "The AWS region into which the resources will be deployed"
   type        = string
